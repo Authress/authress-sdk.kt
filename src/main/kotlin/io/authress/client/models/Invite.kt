05c20a0869a0a3ea8d8218fe79d570e0a771cd46
Warren Parad <gpg@warrenparad.net>
2024-10-29 13:01:02 +0100

    Add invite statement.

--- src/main/kotlin/io/authress/client/models/Invite.kt
+++ src/main/kotlin/io/authress/client/models/Invite.kt
@@ -2,7 +2,7 @@
 package io.authress.client.models
 
 import io.authress.client.models.AccountLinks
-import io.authress.client.models.Statement
+import io.authress.client.models.InviteStatement
 
 /**
  * The user invite used to invite users to your application or to Authress as an admin.
@@ -15,7 +15,7 @@ data class Invite (
     /* The unique identifier for the invite. */
     val inviteId: kotlin.String,
     /* A list of statements which match roles to resources. The invited user will all statements apply to them */
-    val statements: kotlin.Array<Statement>,
+    val statements: kotlin.Array<InviteStatement>,
     val links: AccountLinks
 ) {
 }
